c079d3c549fa63949444539d754ecabed8b0b31a
Jos.c_s <josecarlospc001@gmail.com>
2025-02-24 14:45:44 -0300

    RESOLVI

--- funcoes.c
+++ funcoes.c
@@ -49,7 +49,7 @@ void menuPrincipal()
         default:
             printf("Opcao indisponivel, Tente novamente. \n");
             getchar();
-            getchar();    
+            getchar();
         }
     } while (opc != 0);
 }
@@ -79,14 +79,16 @@ void menuCarros()
             limparTela();
             vizualizarCarros();
             printf("\nPressione ENTER para voltar ao menu");
-            getchar(); getchar();
+            getchar();
+            getchar();
             break;
 
         case 2:
             limparTela();
             incluirCarros();
             printf("\nPressione ENTER para voltar ao menu");
-            getchar(); getchar();
+            getchar();
+            getchar();
             break;
 
         case 0:
@@ -106,10 +108,11 @@ void menuCarros()
     } while (opc != 0);
 }
 
-Carro carros[MAX_CARROS];//Matriz dos carros
+Carro carros[MAX_CARROS]; // Matriz dos carros
 int totalCarros = 0;
 
-void incluirCarros(){
+void incluirCarros()
+{
 
     limparTela();
     char nomeArquivo[TAM_LINHA];
@@ -117,77 +120,89 @@ void incluirCarros(){
     printf("Digite o nome do arquivo de cadastro (ex: carros_registro.txt): ");
     scanf("%s", nomeArquivo);
 
-    //arquivo de entrada
+    // arquivo de entrada
     FILE *arquivoEntrada = fopen(nomeArquivo, "r");
-    if (arquivoEntrada == NULL) {
+    if (arquivoEntrada == NULL)
+    {
         printf("ERRO: arquivo não encontrado");
         return;
     }
 
-    //criando o arquivo de confirmacoa
+    // criando o arquivo de confirmacoa
     FILE *arquivoConfirmacao = fopen("cadastros_realizados.txt", "w");
     fprintf(arquivoConfirmacao, "=======Cadastros Realizados Com Sucesso======\n\n");
-    
+
     char linha[TAM_LINHA];
     int linhaAtual = 0;
     Carro novoCarro;
 
-    while (fgets(linha, TAM_LINHA, arquivoEntrada) != NULL && totalCarros < MAX_CARROS) {
+    while (fgets(linha, TAM_LINHA, arquivoEntrada) != NULL && totalCarros < MAX_CARROS)
+    {
 
-        linha[strcspn(linha, "\n")] = '\0'; //remove quebra de linha
+        linha[strcspn(linha, "\n")] = '\0'; // remove quebra de linha
 
-        switch (linhaAtual % 4) {
-            case 0: strcpy(novoCarro.marca, linha); break;
-            case 1: strcpy(novoCarro.modelo, linha); break;
-            case 2: novoCarro.ano = atoi(linha); break;
-            case 3:
+        switch (linhaAtual % 4)
+        {
+        case 0:
+            strcpy(novoCarro.marca, linha);
+            break;
+        case 1:
+            strcpy(novoCarro.modelo, linha);
+            break;
+        case 2:
+            novoCarro.ano = atoi(linha);
+            break;
+        case 3:
             strcpy(novoCarro.placa, linha);
 
-            //adiciona o carro à matriz
+            // adiciona o carro à matriz
             carros[totalCarros] = novoCarro;
 
-            //escrevendo o arquivo de confirmacao
-            fprintf(arquivoConfirmacao, "Carro %d:\n", ++totalCarros);
-                fprintf(arquivoConfirmacao, "Marca: %s\n", novoCarro.marca);
-                fprintf(arquivoConfirmacao, "Modelo: %s\n", novoCarro.modelo);
-                fprintf(arquivoConfirmacao, "Ano: %d\n", novoCarro.ano);
-                fprintf(arquivoConfirmacao, "Placa: %s\n", novoCarro.placa);
-                fprintf(arquivoConfirmacao, "-----------------------------\n\n");
+            // escrevendo o arquivo de confirmacao
+            fprintf(arquivoConfirmacao, "Carro %d:\n", totalCarros + 1);
+            fprintf(arquivoConfirmacao, "Marca: %s\n", novoCarro.marca);
+            fprintf(arquivoConfirmacao, "Modelo: %s\n", novoCarro.modelo);
+            fprintf(arquivoConfirmacao, "Ano: %d\n", novoCarro.ano);
+            fprintf(arquivoConfirmacao, "Placa: %s\n", novoCarro.placa);
+            fprintf(arquivoConfirmacao, "-----------------------------\n\n");
 
-            totalCarros ++;
+            totalCarros++;
             break;
         }
-        linhaAtual ++;
+        linhaAtual++;
     }
 
     fclose(arquivoEntrada);
     fclose(arquivoConfirmacao);
 
     FILE *arquivoSaida = fopen("carros.txt", "a");
-    for (int i = 0; i < totalCarros; i++) {
-        fprintf(arquivoSaida, "%s\n%s\n%d\n%s\n", 
-                carros[i].marca, 
-                carros[i].modelo, 
-                carros[i].ano, 
+    for (int i = 0; i < totalCarros; i++)
+    {
+        fprintf(arquivoSaida, "%s\n%s\n%d\n%s\n",
+                carros[i].marca,
+                carros[i].modelo,
+                carros[i].ano,
                 carros[i].placa);
     }
     fclose(arquivoSaida);
 
-    printf("\n%d carros cadastrados com sucesso!\n");
+    printf("\n%d carros cadastrados com sucesso!\n", totalCarros);
     printf("Comprovante salvo em: cadastros_realizados.txt\n");
 }
 
+void vizualizarCarros()
+{
 
-void vizualizarCarros(){
-   
     limparTela();
-    if (totalCarros == 0){
+    if (totalCarros == 0)
+    {
         printf("Nenhum Cadastro Efetuado!\n");
         return;
     }
 
     printf("\n========== CARROS CADASTRADOS ==========\n");
-    for (int i = 0; i < totalCarros; i++) {
+    for (int i = 0; i < totalCarros; i++)
+    {
         printf("\nCarro %d:\n", i + 1);
         printf("Marca: %s\n", carros[i].marca);
         printf("Modelo: %s\n", carros[i].modelo);
--- funcoes.h
+++ funcoes.h
@@ -10,27 +10,28 @@
 #define TAM_LINHA 100
 
 //?estrutura do carro
-typedef struct {
+typedef struct
+{
     char marca[TAM_LINHA];
-    char modelo [TAM_LINHA];
+    char modelo[TAM_LINHA];
     int ano;
-    char placa [TAM_LINHA];
+    char placa[TAM_LINHA];
 } Carro;
 
 //?variaveis globais carros
 extern Carro carros[MAX_CARROS];
 extern int totalCarros;
 
-//!FUNCOES
+//! FUNCOES
 void boasVindas(); // exibe a mensagem de boas vindas
 
 void menuPrincipal(); // exibe o menu principal
 
-void menuCarros(); //menu para a aba carros
+void menuCarros(); // menu para a aba carros
 
-void incluirCarros(); //funcao para realizar a inclusao de novos carros
+void incluirCarros(); // funcao para realizar a inclusao de novos carros
 
-void vizualizarCarros(); //funcao para a vizualizaçao dos cadastros ja efetuados
+void vizualizarCarros(); // funcao para a vizualizaçao dos cadastros ja efetuados
 
 void limparTela(); // limpa a tela
 
